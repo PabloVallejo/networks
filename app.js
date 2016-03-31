a2104e144613e31824e43bfd21d291998a66bdfb
Pablo Vallejo <pabloninety@gmail.com>
2016-03-30 19:53:06 -0500

    Handle dynamic port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // Respond to any GET request.
 app.get('*', function(req, res) {
@@ -9,6 +10,6 @@ app.get('*', function(req, res) {
 });
 
 // Have the app listening on port 3000.
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Listening on port 3000!');
 });
