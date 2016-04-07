2dd9b7670ca3105005cc91b27f21824da4d4e445
Pablo Vallejo <pabloninety@gmail.com>
2016-04-06 20:25:03 -0500

    :sunny: Add front page

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// Config.
+app.set('views', 'public/views');
+app.set('view engine', 'ejs');
+
+// Static files
+app.use(express.static('public'));
+
+// Routes.
+app.get('/', function(req, res) {
+    res.render('index', {title: 'sample'});
+});
+
 // Respond to any GET request.
 app.get('*', function(req, res) {
-  res.json({
-    status: 200,
-  });
+    res.status(404).end();
 });
+
 
 // Have the app listening on port 3000.
 app.listen(port, function () {
